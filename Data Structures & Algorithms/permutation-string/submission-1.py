3bf82fdea9d901bab3b946d68630e82372f9c602
Rajeev Ranjan <rajeevranjan939@gmail.com>
2026-06-05 16:35:00 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,32 @@
+# Permutation in String
+# You are given two strings s1 and s2.
+# Return true if s2 contains a permutation of s1, or false otherwise. That means if a permutation of s1 exists as a substring of s2, then return true.
+# Both strings only contain lowercase letters.
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        
+        if not s1: return True
+
+        start = 0
+        s1dict = {}
+        for s in s1:
+            s1dict[s] = s1dict.get(s, 0) + 1
+
+        bag = {}
+        for end in range(len(s2)):
+            bag[s2[end]] = bag.get(s2[end], 0) + 1
+
+            if end - start + 1 > len(s1):
+                if bag[s2[start]] == 1:
+                    del bag[s2[start]]
+                else:
+                    bag[s2[start]] = bag.get(s2[start], 0) - 1
+                start += 1
+
+            if s1dict == bag:
+                return True
+
+        return False

b929b15333e1ccabb0c3e7086ec745f3b716089b
Rajeev Ranjan <rajeevranjan939@gmail.com>
2026-06-19 16:39:32 +0530

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.py
@@ -0,0 +1,24 @@
+# Jump Game II
+# You are given an array of integers nums, where nums[i] represents the maximum length of a jump towards the right from index i. For example, if you are at nums[i], you can jump to any index i + j where:
+
+# j <= nums[i]
+# i + j < nums.length
+# You are initially positioned at nums[0].
+
+# Return the minimum number of jumps to reach the last position in the array (index nums.length - 1). You may assume there is always a valid answer.
+# **** with o(1) space complexity, using window[l,r] instead of array
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l, r = 0, 0
+        res = 0
+
+        while r < len(nums) - 1:  # if r is at last index, break while
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+
+            l, r = r + 1, farthest
+            res += 1
+
+        return res
